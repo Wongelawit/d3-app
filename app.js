d77dbc497516d9abf6cccbbad56c62f73f89d395
Wongelawit Teka Zewde <gospel.teka@gmail.com>
2019-03-13 09:25:08 -0700

    add d3-numerosity

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var numerosity = require ('d3-numerosity');
+var numerosity = require('d3-numerosity');
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-    res.send(d3_numerosity.create());
+    //res.status(200).json({sum: numerosity.create(1,2)})
+    res.status(200).json({sub: numerosity.sub.sub(1,2)})
 });
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(4000, function () {
+    console.log('Example app listening on port 4000!');
 });
